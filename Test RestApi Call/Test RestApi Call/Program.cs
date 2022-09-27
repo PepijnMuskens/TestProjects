b3a04d0a9fec3ebc245e3b8624ef04003a8ff873
Pepijn <437675@student.fontys.nl>
2022-09-27 13:20:12 +0200

    api can controll lights and fade function

--- Test RestApi Call/Test RestApi Call/Program.cs
+++ Test RestApi Call/Test RestApi Call/Program.cs
@@ -13,10 +13,38 @@ string client_id = "fontys";
 string url = "https://staging.strijp.openremote.app";
 string token = getToken();
 string broeinestid = "3lGbluNj94x8A7b3NFieiy";
-string lamp = "3v4kD62VpTAHAmMFW536hQ";
+string lamp1 = "3v4kD62VpTAHAmMFW536hQ";
+string lamp2 = "4ErkQXvRN0b1aFS1z5Mi8t";
 
-PutAttribute("3v4kD62VpTAHAmMFW536hQ", "#100000");
-Console.WriteLine(GetAsset(lamp));
+
+//Reset();
+
+int[] Color = new int[] { 255, 0, 0 };
+for(int i = 0; i < 3; i++)
+{
+    int x = i + 1;
+    if (x == 3) x = 0;
+    
+    for(int j = 0; j <= 255; j = j + 5)
+    {
+        Thread.Sleep(1000);
+        Color[x] = j;
+        ChangeColor(lamp1, Color);
+    }
+    token = getToken();
+    for (int j = 255; j >= 0; j = j -5)
+    {
+        Thread.Sleep(1000);
+        Color[i] = j;
+        ChangeColor(lamp1, Color);
+    }
+    token = getToken();
+
+
+}
+
+//Console.WriteLine(GetAsset(lamp1));
+//Console.WriteLine( "\n" + GetAsset(lamp2));
 
 
 string getToken()
@@ -45,16 +73,41 @@ string GetAsset(string assetid)
     return response.Content.ToString();
 }
 
-void PutAttribute(string assetid, string color)
+void ChangeColor(string assetid, int[] color)
 {
     var client = new RestClient(url + "/api/" + realm + "/asset/" + assetid + "/attribute/colourRgbLed");
     var request = new RestRequest();
-
-    var requestContent = new StringContent("\"value\":\"#100000\"", Encoding.UTF8, "application/json");
-
     request.Method = Method.Put;
-    request.AddBody(requestContent);
+    request.AddBody(color);
     request.AddHeader("authorization", "Bearer " + token);
     Console.WriteLine(client.Execute(request).Content);
     return;
+}
+
+void TurnOn(string assetid)
+{
+    var client = new RestClient(url + "/api/" + realm + "/asset/" + assetid + "/attribute/onOff");
+    var request = new RestRequest();
+    request.Method = Method.Put;
+    request.AddBody(true);
+    request.AddHeader("authorization", "Bearer " + token);
+    Console.WriteLine(client.Execute(request).Content);
+    return;
+}
+
+void TurnOff(string assetid)
+{
+    var client = new RestClient(url + "/api/" + realm + "/asset/" + assetid + "/attribute/onOff");
+    var request = new RestRequest();
+    request.Method = Method.Put;
+    request.AddBody(false);
+    request.AddHeader("authorization", "Bearer " + token);
+    Console.WriteLine(client.Execute(request).Content);
+    return;
+}
+
+void Reset()
+{
+    TurnOff(broeinestid);
+    ChangeColor(broeinestid, new int[] {0,0,0});
 }
